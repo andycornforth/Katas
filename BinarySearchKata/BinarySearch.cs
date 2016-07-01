bbd2fd04c79c463993d06e3a8f3c84e45fcd5066
Andy Cornforth <andycornforth@live.co.uk>
2016-07-01 16:55:31 +0100

    Completed Binary search algorithm. Coding kata. Before refactoring.

---
+++ BinarySearchKata/BinarySearch.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchKata
+{
+    public class BinarySearch
+    {
+        public int SearchFor(int key, int[] array)
+        {
+            if (array.Length.Equals(0))
+                return 0;
+            if (!array.Contains(key))
+                return -1;
+            if (array.Length.Equals(1))
+                return 1;
+
+
+            int low = 0, high = (array.Length - 1);
+
+            var midPoint = (low + high) / 2;
+            int iterations = 1;
+            int currentValue = array[midPoint];
+
+            while (currentValue != key)
+            {
+                if (currentValue > key)//lower half
+                {
+                    high = midPoint - 1;
+                }
+                else//higher half
+                {
+                    low = midPoint + 1;
+                }
+                midPoint = (low + high) / 2;
+                currentValue = array[midPoint];
+                iterations++;
+            }
+
+            return iterations;
+        }
+    }
+}
---
+++ BinarySearchKataTest/BinarySearchTests.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BinarySearchKata;
+
+namespace BinarySearchKataTest
+{
+    [TestClass]
+    public class BinarySearchTests
+    {
+        private BinarySearch _binarySearch;
+
+        [TestInitialize]
+        public void Initialise()
+        {
+            _binarySearch = new BinarySearch();
+        }
+
+
+        [TestMethod]
+        public void WhenArrayIsEmptyReturnZero()
+        {
+            var result = _binarySearch.SearchFor(1, new int[] { });
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void WhenKeyIsNotInArrayReturnMinusOne()
+        {
+            var result = _binarySearch.SearchFor(1, new int[] { 2 });
+
+            Assert.AreEqual(-1, result);
+        }
+
+        [TestMethod]
+        public void WhenKeyIsPresentInAListOfLengthOneReturnsOne()
+        {
+            var result = _binarySearch.SearchFor(1, new int[] { 1 });
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void WhenKeyIsInTheMiddleOfAListOfLengthThreeReturnsOne()
+        {
+            var result = _binarySearch.SearchFor(2, new int[] { 1, 2, 3 });
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void WhenKeyIsInTheFirstOfAListOfLengthThreeItReturnsTwo()
+        {
+            var result = _binarySearch.SearchFor(1, new int[] { 1, 2, 3 });
+
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void WhenKeyIsInTheLastOfAListOfLengthThreeItReturnsTwo()
+        {
+            var result = _binarySearch.SearchFor(3, new int[] { 1, 2, 3 });
+
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void WhenKeyIs8FromAListOfLength10ItReturns2()
+        {
+            var result = _binarySearch.SearchFor(8, new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void WhenKeyIs24OfAListOfLength100ItReturns3()
+        {
+            var result = _binarySearch.SearchFor(24, new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+                                                                11, 12, 13, 14, 15, 16, 17, 18, 19,
+                                                                20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
+                                                                30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
+                                                                40, 41, 42, 43, 44, 45, 46, 47, 48, 49,
+                                                                50, 51, 52, 53, 54, 55, 56, 57, 58, 59,
+                                                                60, 61, 62, 63, 64, 65, 66, 67, 68, 69,
+                                                                70, 71, 72, 73, 74, 75, 76, 77, 78, 79,
+                                                                80, 81, 82, 83, 84, 85, 86, 87, 88, 89,
+                                                                90, 91, 92, 93, 94, 95, 96, 97, 98, 99,
+                                                                100 });
+
+            Assert.AreEqual(7, result);
+        }
+    }
+}
